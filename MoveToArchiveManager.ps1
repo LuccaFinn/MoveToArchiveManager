f0bbef5468ed559fad3d39bec22ac6011397b7f9
Lucca Finn <78115169+LuccaFinn@users.noreply.github.com>
2022-10-14 17:49:36 +0200

    Update MoveToArchiveManager.ps1

--- MoveToArchiveManager.ps1
+++ MoveToArchiveManager.ps1
@@ -1,6 +1,6 @@
 #Projektname: MoveToArchiveManager
 #Lucca Finn Wicke
-#IT.N 
+#
 #Ziel: Nach der Bearbeitung sollen Videodateien bzw. der Projektordner für die Langzeitlagerung auf eine 
 #externe Festplatte verschoben und im Quellordner danach gelöscht werden.
 #Hierbei soll nocheinmal eine überprüfung stattfinden, ob der vorherige Kopiervorgang erfolgreich war. 
