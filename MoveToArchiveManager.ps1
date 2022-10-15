826b668357917b01ff4f147040b2f771e0721757
Lucca Finn <78115169+LuccaFinn@users.noreply.github.com>
2022-10-15 18:26:31 +0200

    Update MoveToArchiveManager.ps1

--- MoveToArchiveManager.ps1
+++ MoveToArchiveManager.ps1
@@ -1,9 +1,9 @@
 #Projektname: MoveToArchiveManager
 #Lucca Finn Wicke
 #
-#Ziel: Nach der Bearbeitung sollen Videodateien bzw. der Projektordner fÃ¼r die Langzeitlagerung auf eine 
-#externe Festplatte verschoben und im Quellordner danach gelÃ¶scht werden.
-#Hierbei soll nocheinmal eine Ã¼berprÃ¼fung stattfinden, ob der vorherige Kopiervorgang erfolgreich war. 
+#Ziel: Nach der Bearbeitung sollen Videodateien bzw. der Projektordner fÃƒÂ¼r die Langzeitlagerung auf eine 
+#externe Festplatte verschoben und im Quellordner danach gelÃƒÂ¶scht werden.
+#Hierbei soll nocheinmal eine ÃƒÂ¼berprÃƒÂ¼fung stattfinden, ob der vorherige Kopiervorgang erfolgreich war. 
 
 
 #0.2 Wir holen uns den Scriptpfad und legen diesen als Quellverzeichnis fest
@@ -32,15 +32,15 @@ $neuerKopierPfad = get-location
 
 
 
-#1.Festlegen der Pfade als Variablen fÃ¼r eine bessere Ãœbersicht und weniger Schreibarbeit
+#1.Festlegen der Pfade als Variablen fÃƒÂ¼r eine bessere ÃƒÅ“bersicht und weniger Schreibarbeit
 
 $PfadA= "$scriptpath"
 $PfadB= "$neuerkopierpfad"
 
-#1.1 Nutzer-Abfrage | Soll das Skript wirklich ausgefÃ¼hrt werden? 
+#1.1 Nutzer-Abfrage | Soll das Skript wirklich ausgefÃƒÂ¼hrt werden? 
 
-#Anfangsabfrage "Dieses Programm kopiert alle Projektdateien in den Archiv-Ordner und lÃ¶scht sie danach im Quellverzeichnis automatisch nach 10 Sekunden" 
-#               "Zum Fortfahren drÃ¼cken Sie bitte "Y"! 
+#Anfangsabfrage "Dieses Programm kopiert alle Projektdateien in den Archiv-Ordner und lÃƒÂ¶scht sie danach im Quellverzeichnis automatisch nach 10 Sekunden" 
+#               "Zum Fortfahren drÃƒÂ¼cken Sie bitte "Y"! 
 
 
 #2.Nutzen von Robocopy um alle Dateien von Pfad A zu Pfad B zu transportieren 
@@ -50,7 +50,7 @@ robocopy $PfadA $PfadB  /MIR;
  $KopierStatus= $LASTEXITCODE
 
 if ($Kopierstatus -eq 1) {write "Das Kopieren wurde erfolgreich abgeschlossen!"
-                            "In 10 Sekunden werden die Daten im Quellordner automatisch gelÃ¶scht!"}
+                            "In 10 Sekunden werden die Daten im Quellordner automatisch gelÃƒÂ¶scht!"}
 
 if ($Kopierstatus  -eq 0) {write "Es wurde nichts kopiert!"}
 
@@ -58,7 +58,13 @@ if ($Kopierstatus -gt 1) {write "Beim Kopieren ist ein Fehler aufgetreten!"
                                 "Der Fehlercode lautet: $Kopierstatus"}
 
 #Was fehlt noch? 
-#Anfangsabfrage "Dieses Programm kopiert alle Projektdateien in den Archiv-Ordner und lÃ¶scht sie danach im Quellverzeichnis automatisch nach 10 Sekunden" 
-#               "Zum Fortfahren drÃ¼cken Sie bitte "Y"! 
+#Anfangsabfrage "Dieses Programm kopiert alle Projektdateien in den Archiv-Ordner und lÃƒÂ¶scht sie danach im Quellverzeichnis automatisch nach 10 Sekunden" 
+#               "Zum Fortfahren drÃƒÂ¼cken Sie bitte "Y"! 
 
 
+
+New-Item -Path "$PfadA" -Name "Dieser Ordner wurde bereits kopiert.txt" -ItemType "file"
+
+#Known-Issues
+
+# Ordner darf keine Leerzeichen enthalten 
